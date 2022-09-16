85895533f57bf478395f854b2d3d421ed57f06da
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-16 20:16:34 +0530

    ADDED:uc1 for Address book system

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class AddressBookMain {
+
+	/**
+	 * UC1-Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...
+	 */
+	public static void main(String[] args) {
+			Contacts person1 = new Contacts("Ananth","Nair","Home","Tn","Mdu",006,9034568,"Ananth@gmail");
+
+	}
+
+}
---
+++ src/com/bridgelabz/Contacts.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class Contacts {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zipCode;
+	private long phoneNumber;
+	private String email;
+	
+	public Contacts(String firstName, String lastName, String address, String city, String state, int zipCode,
+			long phoneNumber, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+	 
+}
